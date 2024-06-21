5d10bbe431e83cc633cb524ae592de433df863d0
俵迫かなめ <k-tawarasako0173@st.kyoto-art.ac.jp>
2024-06-21 15:07:10 +0900

    0621フラグ管理のプログラム、アイテムの配置

--- Assets/Scenes/script/Itembox.cs
+++ Assets/Scenes/script/Itembox.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Itembox : MonoBehaviour
 {
     //Slotsにslot要素をコードから入れる方法
-
     [SerializeField] Slot[] slots;
-    [SerializeField]Slot selectedSlot = null;
+    [SerializeField] Slot selectedSlot = null;
+    [SerializeField] Text notificationText;
 
     //どこでも実行できるやつ(staticはどこでも引っ張っていける変数）
-    public static Itembox instance; 
+    public static Itembox instance;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             slots = GetComponentsInChildren<Slot>();
+        }
 
+        // 初期状態でテキストを非表示にする
+        if (notificationText != null)
+        {
+            notificationText.gameObject.SetActive(false);
         }
     }
 
@@ -32,29 +39,29 @@ public class Itembox : MonoBehaviour
                 break;
             }
         }
-      
     }
 
     public void OnSelectSlot(int position)
     {
         //一旦全てのスロットの選択パネルを非表示
-        foreach(Slot slot in slots)
+        foreach (Slot slot in slots)
         {
             slot.HideBGPanel();
         }
         selectedSlot = null;
+
         //選択されたスロットの選択パネルを表示
-        if(slots[position].OnSelected())
+        if (slots[position].OnSelected())
         {
             selectedSlot = slots[position];
         }
-        
     }
+
     //アイテムの使用を試みる＆使えるなら使ってしまう
     public bool TryUseItem(Item.Type type)
     {
-        //選択スロットがあるか
-        if (selectedSlot == null) 
+         //選択スロットがあるか
+        if (selectedSlot == null)
         {
             return false;
         }
@@ -65,16 +72,24 @@ public class Itembox : MonoBehaviour
             selectedSlot = null;
             return true;
         }
-        return false;
+        else
+        {
+            // アイテムが間違っている場合にテキストを表示
+            if (notificationText != null)
+            {
+                notificationText.text = "このアイテムではないみたい";
+                notificationText.gameObject.SetActive(true); // テキストを表示
+            }
+            return false;
+        }
     }
 
     public Item GetSelectedItem()
     {
-        if(selectedSlot == null)
+        if (selectedSlot == null)
         {
             return null;
         }
         return selectedSlot.GetItem();
     }
 }
-
