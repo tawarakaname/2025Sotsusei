4c8e546aa842a3f4e688f0e2b14fbf6c508b3d75
俵迫かなめ <k-tawarasako0173@st.kyoto-art.ac.jp>
2024-09-18 19:50:04 +0900

    かなめ/motosenのカメラ変更と元栓を開ける動作の間のフラグ

--- Assets/script/ObjFlag/MotosenFlagOn.cs
+++ Assets/script/ObjFlag/MotosenFlagOn.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MotosenFlagOn : MonoBehaviour
 {
     [SerializeField] Collider triggerCollider; // 追加したコライダーのフィールド
     private bool playerInsideCollider = false;
+    private bool isFireButtonPressed = false; // 丸ボタンの押下を管理
+    private FlagManager flagManager; // フラグマネージャーのインスタンスをキャッシュ
 
-    
+    private void Start()
+    {
+        flagManager = FlagManager.Instance; // FlagManagerのインスタンスを取得
+    }
+
     void Update()
     {
+        // CameraZoomObjFlagがfalseなら早めに処理を終了
+        if (!flagManager.GetFlag(FlagManager.FlagType.CameraZoomObj)) return;
+
         if (playerInsideCollider)
         {
-            // PS4コントローラーの⚪︎ボタンは「Fire2」として認識されます
-            if (Input.GetButtonDown("Fire2"))
-            {
-                OnClickObj();
-
-            }
+            HandleFireButtonInput();
         }
     }
+
+    // 丸ボタンの入力処理
+    private void HandleFireButtonInput()
+    {
+        // 丸ボタンが押された時の処理
+        if (Input.GetButtonDown("Fire2") && !isFireButtonPressed)
+        {
+            OnClickObj();
+            isFireButtonPressed = true; // ボタンが押されたらフラグをセット
+        }
+        else if (!Input.GetButton("Fire2"))
+        {
+            // ボタンが離されたらフラグをリセット
+            isFireButtonPressed = false;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -41,5 +61,6 @@ public class MotosenFlagOn : MonoBehaviour
     {
         // フラグを設定する
         FlagManager.Instance.SetFlag(FlagManager.FlagType.Motosen, true);
+        Debug.Log("Motosen Flag On");
     }
 }
