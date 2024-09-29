3b2b63989f1ad73dd52b7361bea70b2f09c4fec1
俵迫かなめ <k-tawarasako0173@st.kyoto-art.ac.jp>
2024-09-30 00:11:27 +0900

    かなめ/炎のエフェクト調整、元栓アニメーション

--- Assets/script/Fire Particle.cs
+++ Assets/script/Fire Particle.cs
@@ -10,22 +10,33 @@ public class FireParticle : MonoBehaviour
     [SerializeField] private GameObject FireparticleD;
     private FlagManager flagManager;
 
+    private ParticleSystem particleSystemA;
+    private ParticleSystem particleSystemB;
+    private ParticleSystem particleSystemC;
+    private ParticleSystem particleSystemD;
+
     void Start()
     {
         flagManager = FlagManager.Instance; // FlagManagerのインスタンスを取得
 
+        // ParticleSystem コンポーネントを取得
+        if (FireparticleA != null) particleSystemA = FireparticleA.GetComponent<ParticleSystem>();
+        if (FireparticleB != null) particleSystemB = FireparticleB.GetComponent<ParticleSystem>();
+        if (FireparticleC != null) particleSystemC = FireparticleC.GetComponent<ParticleSystem>();
+        if (FireparticleD != null) particleSystemD = FireparticleD.GetComponent<ParticleSystem>();
+
         // 初期状態でFireparticleを非表示に設定
-        if (FireparticleA != null) FireparticleA.SetActive(false);
-        if (FireparticleB != null) FireparticleB.SetActive(false);
-        if (FireparticleC != null) FireparticleC.SetActive(false);
-        if (FireparticleD != null) FireparticleD.SetActive(false);
+        SetParticleActive(FireparticleA, false);
+        SetParticleActive(FireparticleB, false);
+        SetParticleActive(FireparticleC, false);
+        SetParticleActive(FireparticleD, false);
     }
 
     void Update()
     {
-        // DialPasswordFlagを使用
         bool isDialPasswordclearFlagOn = flagManager.GetFlag(FlagManager.FlagType.DialPasswordclear);
         bool isFireParticleONFlagOn = flagManager.GetFlag(FlagManager.FlagType.FireParticleON);
+        bool isColorPasswordclearFlagOn = flagManager.GetFlag(FlagManager.FlagType.ColorPasswordclear);
 
         SetParticleActive(FireparticleA, isDialPasswordclearFlagOn);
         SetParticleActive(FireparticleB, isDialPasswordclearFlagOn);
@@ -34,23 +45,31 @@ public class FireParticle : MonoBehaviour
 
         if (isDialPasswordclearFlagOn && !isFireParticleONFlagOn)
         {
-            // フラグを設定する
             FlagManager.Instance.SetFlag(FlagManager.FlagType.FireParticleON, true);
             Debug.Log("FireParticleFlagON");
+        }
+
+        // colorpasswordclear フラグがオンになったら色を変更
+        if (isColorPasswordclearFlagOn)
+        {
+            ChangeParticleColor(particleSystemA, Color.yellow);              // Aは黄色
+            ChangeParticleColor(particleSystemB, new Color(0.3f, 1f, 0.5f));  // Bはライムグリーン
+            ChangeParticleColor(particleSystemC, new Color(1f, 0.4f, 0.7f)); // Cはピンク
+            ChangeParticleColor(particleSystemD, new Color(1f, 0.5f, 0f));   // Dはオレンジ
         }
     }
 
     private void SetParticleActive(GameObject particle, bool isActive)
     {
         if (particle == null) return;
+        particle.SetActive(isActive);
+    }
 
-        if (isActive && !particle.activeSelf)
-        {
-            particle.SetActive(true);
-        }
-        else if (!isActive && particle.activeSelf)
-        {
-            particle.SetActive(false);
-        }
+    private void ChangeParticleColor(ParticleSystem particleSystem, Color newColor)
+    {
+        if (particleSystem == null) return;
+
+        var mainModule = particleSystem.main;
+        mainModule.startColor = newColor;
     }
 }
--- Assets/script/ObjFlag/MotosenFlagOn.cs
+++ Assets/script/ObjFlag/MotosenFlagOn.cs
@@ -9,6 +9,9 @@ public class MotosenFlagOn : MonoBehaviour
     private bool isFireButtonPressed = false; // 丸ボタンの押下を管理
     private FlagManager flagManager; // フラグマネージャーのインスタンスをキャッシュ
 
+    public GameObject huta; // motosenhuta を指定するためのフィールド
+    private bool isRotating = false; // 一度だけ回転させるためのフラグ
+
     private void Start()
     {
         flagManager = FlagManager.Instance; // FlagManagerのインスタンスを取得
@@ -29,7 +32,7 @@ public class MotosenFlagOn : MonoBehaviour
     private void HandleFireButtonInput()
     {
         // 丸ボタンが押された時の処理
-        if (Input.GetButtonDown("Fire2") && !isFireButtonPressed)
+        if (Input.GetButtonDown("Fire2") && !isFireButtonPressed && !isRotating)
         {
             OnClickObj();
             isFireButtonPressed = true; // ボタンが押されたらフラグをセット
@@ -62,5 +65,33 @@ public class MotosenFlagOn : MonoBehaviour
         // フラグを設定する
         FlagManager.Instance.SetFlag(FlagManager.FlagType.Motosen, true);
         Debug.Log("Motosen Flag On");
+
+        // motosenhutaを滑らかにY方向に90度回転させる
+        if (huta != null && !isRotating)
+        {
+            StartCoroutine(RotateHutaSmoothly());
+        }
+    }
+
+    // コルーチンで滑らかに回転させる
+    private IEnumerator RotateHutaSmoothly()
+    {
+        isRotating = true;
+        float currentRotation = 0f; // 現在の回転角度
+        float targetRotation = 90f; // 目標の回転角度
+        float rotationSpeed = 90f; // 1秒で90度回転するスピード
+
+        while (currentRotation < targetRotation)
+        {
+            float step = rotationSpeed * Time.deltaTime; // フレームごとの回転量
+            huta.transform.Rotate(0, step, 0); // Y軸に回転
+            currentRotation += step;
+            yield return null; // 次のフレームまで待つ
+        }
+
+        // 最後に90度に正確にセットする
+        huta.transform.rotation = Quaternion.Euler(0, 90, 0);
+        Debug.Log("motosenhutaが90度に回転しました");
+        isRotating = false;
     }
 }
